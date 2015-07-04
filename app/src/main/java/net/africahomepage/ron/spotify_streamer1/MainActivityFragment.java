85ade66f9b5534fb8dc8e16b06fac7e76388c2af
ron <ronald.marangwanda@gmail.com>
2015-07-04 09:15:50 -0400

    added internet permission to manifest

--- app/src/main/java/net/africahomepage/ron/spotify_streamer1/MainActivityFragment.java
+++ app/src/main/java/net/africahomepage/ron/spotify_streamer1/MainActivityFragment.java
@@ -3,13 +3,13 @@ package net.africahomepage.ron.spotify_streamer1;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -39,12 +39,12 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        EditText searchEditText = (EditText) root.findViewById(R.id.search_editText);
+        TextView searchEditText = (TextView) root.findViewById(R.id.search_editText);
         searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ) {
                     String searchText = v.getText().toString();
                     FetchMusicTask fetchMusicTask = new FetchMusicTask();
                     fetchMusicTask.execute(searchText);
@@ -65,7 +65,7 @@ public class MainActivityFragment extends Fragment {
                 Arrays.asList(artists)
         );
 
-        mSpotifyadapter = new ArrayAdapter<String>(getActivity(),R.layout.main_listview_textview,R.id.listview_textview,data);
+        mSpotifyadapter = new ArrayAdapter<>(getActivity(),R.layout.main_listview_textview, R.id.artist_name_textview ,data);
 
         ListView listView = (ListView) root.findViewById(R.id.mainActivity_listView);
         listView.setAdapter(mSpotifyadapter);
@@ -75,7 +75,7 @@ public class MainActivityFragment extends Fragment {
 
     public class FetchMusicTask extends AsyncTask<String, Void, ArtistsPager> {
 
-        @Override
+        private final String LOG_TAG =  FetchMusicTask.class.getSimpleName();
         protected void onPostExecute(ArtistsPager result) {
            mArtistData = result;
         }
@@ -85,6 +85,7 @@ public class MainActivityFragment extends Fragment {
             SpotifyApi api = new SpotifyApi();
             SpotifyService spotify = api.getService();
             ArtistsPager artistData = spotify.searchArtists(artist[0]);
+            Log.v(LOG_TAG,artistData.toString());
             return artistData;
         }
 
