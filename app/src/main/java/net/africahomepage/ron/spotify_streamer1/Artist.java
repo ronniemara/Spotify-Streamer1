895f9666eaf5166158987219a532e4268c0c94c9
ron <ronald.marangwanda@gmail.com>
2015-07-07 03:51:12 -0400

    added internet permission to manifest

---
+++ app/src/main/java/net/africahomepage/ron/spotify_streamer1/Artist.java
@@ -0,0 +1,12 @@
+package net.africahomepage.ron.spotify_streamer1;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by ron on 07/07/15.
+ */
+public class Artist {
+    public String name;
+    public int SpotifyId;
+    public Picasso imageObject;
+}
--- app/src/main/java/net/africahomepage/ron/spotify_streamer1/MainActivityFragment.java
+++ app/src/main/java/net/africahomepage/ron/spotify_streamer1/MainActivityFragment.java
@@ -14,10 +14,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 /**
@@ -26,7 +27,7 @@ import kaaes.spotify.webapi.android.models.ArtistsPager;
 public class MainActivityFragment extends Fragment {
 
     ArrayAdapter<String> mSpotifyadapter = null;
-    ArtistsPager mArtistData = null;
+    List<Artist> mArtistData = null;
 
 
     public MainActivityFragment() {
@@ -53,17 +54,21 @@ public class MainActivityFragment extends Fragment {
                 return handled;
             }
         });
-        String[] artists = {
-                "Beyonce",
-                "Gucci Mane",
-                "Mariah Carey"
-        };
+//        String[] artists = {
+//                "Beyonce",
+//                "Gucci Mane",
+//                "Mariah Carey"
+//        };
 
 
 
-        ArrayList<String> data = new ArrayList<>(
-                Arrays.asList(artists)
-        );
+//        ArrayList<String> data = new ArrayList<>(
+//                Arrays.asList(artists)
+//        );
+
+        if(mArtistData != null) {
+
+        }
 
         mSpotifyadapter = new ArrayAdapter<>(getActivity(),R.layout.main_listview_textview, R.id.artist_name_textview ,data);
 
@@ -73,20 +78,31 @@ public class MainActivityFragment extends Fragment {
         return root;
     }
 
-    public class FetchMusicTask extends AsyncTask<String, Void, ArtistsPager> {
+    public class FetchMusicTask extends AsyncTask<String, Void, List<Artist>> {
 
         private final String LOG_TAG =  FetchMusicTask.class.getSimpleName();
-        protected void onPostExecute(ArtistsPager result) {
+        protected void onPostExecute(List<Artist> result) {
            mArtistData = result;
         }
 
         @Override
-        protected ArtistsPager doInBackground(String... artist) {
+        protected List<Artist> doInBackground(String... artist) {
             SpotifyApi api = new SpotifyApi();
             SpotifyService spotify = api.getService();
             ArtistsPager artistData = spotify.searchArtists(artist[0]);
-            Log.v(LOG_TAG,artistData.toString());
-            return artistData;
+            List<Artist> artistList = artistData.artists.items;
+            ArrayList<String> result = null;
+            for (int i=0; i < artistList.size(); i++) {
+                Artist art = artistList.get(i);
+
+//                Log.v(LOG_TAG, art.name);
+                try {
+                    result.add(art.name);
+                } catch (Exception e) {
+
+                }
+            }
+            return artistList;
         }
 
     }
