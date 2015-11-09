b14d6e3e74274315a931bac22cac930979c6317f
ronniemara <ronald.marangwanda@gmail.com>
2015-11-08 19:11:39 -0500

    changed detailsAdapter from ListView to RecyclerView

--- app/src/main/java/net/africahomepage/ron/spotify_streamer1/DetailsAdapter.java
+++ app/src/main/java/net/africahomepage/ron/spotify_streamer1/DetailsAdapter.java
@@ -1,6 +1,7 @@
 package net.africahomepage.ron.spotify_streamer1;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,33 +11,64 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by ron on 07/07/15.
  */
-public class DetailsAdapter extends ArrayAdapter<TrackObject>{
+public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.TrackViewHolder>{
 
-    public DetailsAdapter(Context context, List<TrackObject> trackObjects) {
-        super(context, 0, trackObjects );
+    ArrayList<TrackObject> mDataSet;
+
+    public DetailsAdapter(ArrayList<TrackObject> item) {
+        mDataSet = item;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        TrackObject current  = getItem(position);
+    public TrackViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.details_listview_layout, parent, false);
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.details_listview_layout, parent, false);
 
-        ImageView imageview = (ImageView) rootView.findViewById(R.id.details_listview_layout_imageview);
-        Picasso.with(getContext()).load(current.mTrackSmallImageUrl).into(imageview);
+        TrackViewHolder viewHolder = new TrackViewHolder(rootView);
 
-        TextView trackTitle = (TextView) rootView.findViewById(R.id.details_listview_layout_song);
-        trackTitle.setText(current.mTrackTitle);
+        return  viewHolder;
+    }
 
-        TextView trackAlbum = (TextView) rootView.findViewById(R.id.details_listview_layout_album);
-        trackAlbum.setText(current.mTrackAlbum);
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
 
-        return  rootView;
+    @Override
+    public void onBindViewHolder(TrackViewHolder holder, int position) {
+
+        TrackObject item = mDataSet.get(position);
+
+        Picasso.with(holder.container.getContext())
+                .load(item.mTrackSmallImageUrl)
+                .into(holder.imageView);
+
+        holder.trackTitle.setText(item.mTrackTitle);
+
+        holder.trackAlbum.setText(item.mTrackAlbum);
+    }
+
+    public class TrackViewHolder extends RecyclerView.ViewHolder {
+
+        protected View container;
+        protected ImageView imageView;
+        protected TextView trackTitle;
+        protected TextView trackAlbum;
+
+        public TrackViewHolder(View itemView) {
+            super(itemView);
+
+            container = itemView;
+            imageView = (ImageView) itemView.findViewById(R.id.details_listview_layout_imageview);
+            trackTitle = (TextView) itemView.findViewById(R.id.details_listview_layout_song);
+            trackAlbum = (TextView) itemView.findViewById(R.id.details_listview_layout_album);
+        }
     }
 
 }
