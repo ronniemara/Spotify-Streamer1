3bf87090324271625c09ef70b492c884be281b87
ronniemara <ronald.marangwanda@gmail.com>
2015-11-13 18:13:15 -0500

    added mediaplayer OnErrorListener

---
+++ app/src/main/java/net/africahomepage/ron/spotify_streamer1/MediaPlayerOnErrorListener.java
@@ -0,0 +1,12 @@
+package net.africahomepage.ron.spotify_streamer1;
+
+import android.media.MediaPlayer.OnErrorListener;
+
+class MediaPlayerOnErrorListener extends OnErrorListener {
+
+  @Override
+  public boolean OnError(MediaPlayer mp, int what, int extra) {
+    
+    return true;
+  }
+}
